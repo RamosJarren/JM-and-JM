fc7260d8fe78eb2aa4ac3739dbc4d9a4be2f0d45
Mendoza-JM <123426445+Mendoza-JM@users.noreply.github.com>
2023-04-20 07:39:56 +0800

    Update LabAct3-2 Text Field.py

--- LabAct3-2 Text Field.py
+++ LabAct3-2 Text Field.py
@@ -2,15 +2,15 @@ from tkinter import *
 
 class GUI:
     def __init__(self,window):
-        self.txtfld1 = Entry(window, border = "2", width = "15")
-        self.txtfld1.place(x = "40", y = "30", height = "40")
+        self.txtfld1 = Entry(window, border = "2", width = "24")
+        self.txtfld1.place(x = "80", y = "20", height = "40")
         self.txtfld1.insert(0, "This is where I type my text")
 
 
 
 window = Tk()
 wind = GUI(window)
-window.geometry("230x80")
+window.geometry("300x100")
 window.title("Text Field")
 
 window.mainloop()
