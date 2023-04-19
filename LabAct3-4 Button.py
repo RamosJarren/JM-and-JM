280bbfcc268d3dba0a8303182b1bed84e3fde301
Mendoza-JM <123426445+Mendoza-JM@users.noreply.github.com>
2023-04-20 07:52:18 +0800

    Update LabAct3-4 Button.py

--- LabAct3-4 Button.py
+++ LabAct3-4 Button.py
@@ -3,13 +3,13 @@ from tkinter import *
 class GUI:
     def __init__(self,window):
         self.button1 = Button(window, text = "Color", background = "#0000ff", activebackground = "#ffff00", foreground = "#ff0000", activeforeground = "#ff0000")
-        self.button1.place(x = "30", y = "70")
+        self.button1.place(x = "30", y = "130")
         self.button2 = Button(window, text="<---Click to change the color of the button")
-        self.button2.place(x="70", y="70")
+        self.button2.place(x="80", y="130")
 
 window = Tk()
 wind = GUI(window)
-window.geometry("360x120")
+window.geometry("340x180")
 window.title("Button")
 
 window.mainloop()
